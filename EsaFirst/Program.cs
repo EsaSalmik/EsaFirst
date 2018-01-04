7bd0ce96c4e02324f87aab0613c5c0cc8b97b88e
EsaSalmik <esa.salmikangas@jamk.fi>
2018-01-04 13:54:10 +0200

    Test git

--- EsaFirst/Program.cs
+++ EsaFirst/Program.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 /// <summary>
 /// Esa's Small Test for creating and testing GitHub repository in VS2015 and VS2017
 /// last edit: 4.1.2018
-/// tested in VS2017 4.1.2018
+/// tested in VS2017@ZBook and VS2017@D436 4.1.2018
 /// </summary>
 namespace EsaFirst
 {
